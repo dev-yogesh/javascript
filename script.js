a0da7eeb64e7a36cd8d1deeb3c9695e2caa902d7
yogesh-wemotive <yogesh@wemotiveforge.com>
2022-04-18 10:21:18 +0530

    add: removeDuplicateCharacters

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Program');
+const removeDuplicateCharacters = (text) => {
+  const result = text
+    .split('')
+    .filter((char, index, arr) => arr.indexOf(char) === index)
+    .join('');
+
+  return result;
+};
+
+console.log(removeDuplicateCharacters('yogi aadiyogi mahayogi'));
