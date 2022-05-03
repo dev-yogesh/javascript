7f2a0aa02df9aa25ebe163187e9172ed1f10003b
yogesh-wemotive <yogesh@wemotiveforge.com>
2022-05-03 09:03:20 +0530

    add: tripletsSumTarget

---
+++ tripletsSumTarget.js
@@ -0,0 +1,46 @@
+/*
+Way 1 - nested loops, T ==> O(N^3)
+==============================================================================
+const tripletsSumZero = (arr, sumTarget) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[i] + arr[j] + arr[k] === sumTarget) {
+          return [arr[i], arr[j], arr[k]];
+        }
+      }
+    }
+  }
+};
+
+console.log(tripletsSumZero([13, 9, -6, 4, 6, 0], 4));
+==============================================================================
+*/
+
+/*
+Way 2 - pointer approach
+==============================================================================
+const tripletsSumZero = (arr, sumTarget) => {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      sum = arr[i] + arr[left] + arr[right];
+
+      if (sum === sumTarget) {
+        return [arr[i], arr[left], arr[right]];
+      } else if (sum < sumTarget) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+};
+
+console.log(tripletsSumZero([13, 9, -6, 4, 6, 0], 4));
+==============================================================================
+*/
