ec95cb263def3dc18166b6de895254a47de5d54b
yogesh-wemotive <yogesh@wemotiveforge.com>
2022-05-09 11:21:57 +0530

    add: code cleanup

--- AsyncProgramming/promices.js
+++
@@ -1,8 +0,0 @@
-let myPromice = new Promise((resole, reject) => {
-  if (resole) {
-    console.log("Success");
-  } else {
-    reject;
-  }
-});
-myPromice.then(console.log("Hi")).catch(console.log("Ho"));
--- Destructuring/note.js
+++
@@ -1,2 +0,0 @@
-// Destructuring is a convenient way of extracting multiple values from data stored in object and arrays.
-// It makes it possible to unpack values from array, or properties from object into distinct variables.
